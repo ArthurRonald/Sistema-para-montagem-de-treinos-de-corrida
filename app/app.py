ab5a321636e2c093ab9072b01069c6ecff8b759c
Sarah-OAlbuquerque7 <saraho.albuquerque7@gmail.com>
2025-05-28 20:58:05 -0300

    criação da página inicial

--- app/app.py
+++ app/app.py
@@ -1,1 +1,15 @@
 import streamlit as st
+import pagina_inicial
+import pagina_questionario
+
+
+paginas = {"Sobre o Site": [
+    st.Page("pagina_inicial.py", title="Página Inicial", default=True)],
+    "Você": [
+        st.Page("pagina_questionario.py", title="Questionário")
+]
+
+}
+
+pg = st.navigation(paginas)
+pg.run()
---
+++ app/pagina_inicial.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+col1, col2, col3, col4 = st.columns([1, 1, 2, 1])
+with col3:
+    st.title("MZRun")
+
+st.markdown(""" Este site foi criado para ajudar pessoas que desejam melhorar sua rotina de treinos de corrida. A partir de algumas informações simples, ele oferece sugestões personalizadas para tornar seus treinos mais organizados e eficientes. A proposta é tornar o processo de planejar, acompanhar e ajustar seus treinos algo prático, acessível e pensado para o seu ritmo. """)
+
+st.subheader("O que você encontra aqui: ")
+st.markdown("""
+            
+**-Um sistema simples e interativo:** Você só precisa preencher um formulário com algumas informações básicas.
+
+**-Sugestões de treinos personalizadas:** Com base no que você responder, o sistema oferece um plano de corrida adaptado ao seu perfil.
+
+**-Sem necessidade de baixar nada:** Tudo acontece na própria página, de forma prática e leve.
+
+**-Possibilidade de dar feedback:** Depois de seguir os treinos, você pode contar como foi e receber um novo plano ajustado conforme o seu progresso ou suas dificuldades.
+
+**-Lista de metas para te ajudar a se organizar:** O sistema também exibe os treinos da semana de forma simples, como uma lista que você pode acompanhar dia a dia.
+
+**-PDF com seu plano:** Se quiser, você pode salvar ou imprimir seu plano para acompanhar quando estiver offline. """)
+
+st.subheader("Vamos Começar❔🏃‍♂️")
+
+if st.button("Criar seu Treino"):
+    st.switch_page("pagina_questionario.py")
