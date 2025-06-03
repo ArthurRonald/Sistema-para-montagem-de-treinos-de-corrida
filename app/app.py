e53201d9ee4eb4e63600d31a1932606d1ba482ec
Sarah-OAlbuquerque7 <saraho.albuquerque7@gmail.com>
2025-06-03 17:13:23 -0300

    Adição das outras paginas

--- app/app.py
+++ app/app.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import pagina_inicial
+import pagina_perfil
 import pagina_questionario
+import pagina_toDoList
+import pagina_questionario2
 
 
 paginas = {"Sobre o Site": [
-    st.Page("pagina_inicial.py", title="Página Inicial", default=True)],
+    st.Page("pagina_inicial.py", title="Inicio", default=True)],
     "Você": [
-        st.Page("pagina_questionario.py", title="Questionário")
-]
-
+        st.Page("pagina_perfil.py", title="Perfil"),
+        st.Page("pagina_questionario.py", title="Questionário"),
+        st.Page("pagina_toDoList.py", title="Check-List"),
+        st.Page("pagina_questionario2.py", title="Avaliação do Treino")
+],
+    "  ": [st.Page("pagina_final.py", title="FIM")]
 }
 
 pg = st.navigation(paginas)
--- app/pagina_questionario.py
+++ app/pagina_questionario.py
@@ -3,55 +3,60 @@ import streamlit as st
 
 # st.set_page_config(page_title=" Projeto Corrida", page_icon="🏃‍♂️")
 
-st.sidebar.title("Você")
-menu = st.sidebar.selectbox(
-    "Escolha uma opção", ["Cadastro", "Treinos", "To-Do List", "Acompanhamento"])
+# st.sidebar.title("Você")
+# menu = st.sidebar.selectbox(
+# "Escolha uma opção", ["Cadastro", "Treinos", "To-Do List", "Acompanhamento"])
 
 
-if menu == "Cadastro":
-    st.title("🏃‍♂️ Cadastro do Corredor")
+# if menu == "Cadastro":
+# st.title("🏃‍♂️ Cadastro do Corredor")
 
-    nome = st.text_input("Nome")
-    idade = st.number_input("Idade", min_value=10, max_value=100, step=1)
-    peso = st.number_input("Peso (kg)", min_value=30.0,
-                           max_value=200.0, step=0.1)
-    altura = st.number_input(
-        "Altura (cm)", min_value=130.0, max_value=220.0, step=0.1)
+nome = st.text_input("Nome")
+idade = st.number_input("Idade", min_value=10, max_value=100, step=1)
+peso = st.number_input("Peso (kg)", min_value=30.0,
+                       max_value=200.0, step=1.0)
+altura = st.number_input(
+    "Altura (cm)", min_value=130.0, max_value=220.0, step=0.1)
 
-    objetivo = st.selectbox(
-        "Objetivo",
-        ["Perder peso", "Melhorar pace", "Completar 5km",
-            "Completar 10km", "Meia maratona (21km)", "Maratona(42km)"]
-    )
+atividadeSemana = st.slider(
+    "Quantos dias por semana você realiza atividades físicas?", min_value=1, max_value=7)
 
-    nivel = st.selectbox(
-        "Nível de corrida",
-        ["Iniciante", "Intermediário", "Avançado"]
-    )
+tempo = st.number_input(
+    "Qual o tempo máximo, EM MINUTOS, que você já fez em uma corrida ?", min_value=0, max_value=180, step=5)
 
-    dias = st.slider("Quantos dias por semana quer treinar?",
-                     min_value=1, max_value=7)
+distancia = st.number_input(
+    "Qual sua distância máxima, EM QUILÔMETROS , percorrida em uma corrida ?", min_value=0, max_value=100, step=1)
 
-    if st.button("Salvar Cadastro"):
-        if nome == "":
-            st.warning("Por favor, preencha seu nome.")
-        else:
-            st.success(f"Cadastro salvo com sucesso! Bem-vindo, {nome}! 🎉")
-            st.session_state["usuario"] = {
-                "nome": nome,
-                "idade": idade,
-                "peso": peso,
-                "altura": altura,
-                "objetivo": objetivo,
-                "nivel": nivel,
-                "dias_por_semana": dias
-            }
+lesao = st.selectbox("Já sofreu algum tipo de lesão?", ["Sim", "Não"])
 
-            st.write("### Seus dados:")
-            st.json(st.session_state["usuario"])
+objetivo = st.selectbox(
+    "Objetivo",
+    ["Perder peso", "Melhorar pace", "Completar 5km",
+     "Completar 10km", "Meia maratona (21km)", "Maratona(42km)"]
+)
 
-elif menu == "Treinos":
-    st.title("  Agenda de Treinos")
+dias = st.slider("Quantos dias por semana quer treinar?",
+                 min_value=1, max_value=7)
+
+if st.button("Salvar Cadastro"):
+    if nome == "":
+        st.warning("Por favor, preencha seu nome.")
+    else:
+        st.success(f"Cadastro salvo com sucesso! Bem-vindo, {nome}! 🎉")
+        st.session_state["usuario"] = {
+            "nome": nome,
+            "idade": idade,
+            "peso": peso,
+            "altura": altura,
+            "objetivo": objetivo,
+            "dias_por_semana": dias
+        }
+
+        st.write("### Seus dados:")
+        st.json(st.session_state["usuario"])
+
+# elif menu == "Treinos":
+    # st.title("  Agenda de Treinos")
 
     if "usuario" not in st.session_state:
         st.write("Por favor, preencha seu cadastro.")
@@ -59,5 +64,4 @@ elif menu == "Treinos":
         usuario = st.session_state["usuario"]
         st.subheader(f"Treino para {usuario['nome']}")
         st.write(f"🎯 Objetivo: {usuario['objetivo']}")
-        st.write(f"  Nível: {usuario['nivel']}")
         st.write(f"📅 Dias por semana: {usuario['dias_por_semana']}")
