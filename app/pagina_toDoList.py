11e8bfe046ee8c47eaee12388dbd6e49564a181c
Caio César <1caioccss@gmail.com>
2025-06-20 20:10:24 -0300

    tentando exibir os treinos

--- app/pagina_toDoList.py
+++ app/pagina_toDoList.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 st.title("Confira o treino ideal para o nível da atividade desejada")
 
-# Pega os dados do session_state
 previsao = st.session_state.get("previsao")
 historico_lesao = st.session_state.get(
     "dados_usuario", {}).get("lesao_encoded")
@@ -13,28 +12,27 @@ if previsao is None or historico_lesao is None:
         "❌ Dados do usuário não encontrados. Por favor, preencha o questionário primeiro.")
 else:
     st.subheader(
-        f"O nível adequado para sua atividade é **{str(previsao).upper()}**")
+        f"O nível adequado para sua atividade é {str(previsao).upper()}")
 
     if previsao == "avancado" and historico_lesao == "Não":
         treino = pd.read_csv("AVANÇADO SEM LESÃO.csv")
-        st.dataframe(treino)
-
+        st.write(treino)
     elif previsao == "avancado" and historico_lesao == "Sim":
         treino = pd.read_csv("AVANÇADO COM LESÃO.csv")
-        st.dataframe(treino)
+        st.write(treino)
 
     elif previsao == "intermediario" and historico_lesao == "Não":
         treino = pd.read_csv("INTERMEDIÁRIO SEM LESÃO.csv")
-        st.dataframe(treino)
+        st.write(treino)
 
     elif previsao == "intermediario" and historico_lesao == "Sim":
         treino = pd.read_csv("INTERMEDIÁRIO COM LESÃO.csv")
-        st.dataframe(treino)
+        st.write(treino)
 
     elif previsao == "iniciante" and historico_lesao == "Não":
         treino = pd.read_csv("INICIANTE SEM LESÃO.csv")
-        st.dataframe(treino)
+        st.write(treino)
 
     elif previsao == "iniciante" and historico_lesao == "Sim":
         treino = pd.read_csv("INICIANTE COM LESÃO.csv")
-        st.dataframe(treino)
+        st.write(treino)
