55f14d43b0dff0b1a49f90be369ac7997f677b5b
Caio César <1caioccss@gmail.com>
2025-06-20 21:36:39 -0300

    Update pagina_toDoList.py

--- app/pagina_toDoList.py
+++ app/pagina_toDoList.py
@@ -8,36 +8,40 @@ previsao = st.session_state.get("previsao")
 historico_lesao = st.session_state.get(
     "dados_usuario", {}).get("lesao_encoded")
 
-previsto= previsao.inverse_transform(previsao)
+if previsao==0:
+    previsao="Avançado"
+elif previsao==1:
+    previsao= "Iniciante"
+else: previsao="Intermediário"
 
 if previsao is None or historico_lesao is None:
     st.error(
         "❌ Dados do usuário não encontrados. Por favor, preencha o questionário primeiro.")
 else:
     st.subheader(
-        f"O nível adequado para sua atividade é {str(previsto).upper()}")
+        f"O nível adequado para sua atividade é {str(previsao).upper()}")
 
-    if previsao == "avancado" and historico_lesao == "Não":
+    if previsao == "Avançado" and historico_lesao == "Não":
         treino = pd.read_csv("AVANÇADO SEM LESÃO.csv")
         st.write(treino)
-    elif previsao == "avancado" and historico_lesao == "Sim":
+    elif previsao == "Avançado" and historico_lesao == "Sim":
         treino = pd.read_csv("AVANÇADO COM LESÃO.csv")
         st.write(treino)
 
     st.button("Recomendação Inteligente")
 
-    elif previsao == "intermediario" and historico_lesao == "Não":
+    elif previsao == "Intermediario" and historico_lesao == "Não":
         treino = pd.read_csv("INTERMEDIÁRIO SEM LESÃO.csv")
         st.write(treino)
 
-    elif previsao == "intermediario" and historico_lesao == "Sim":
+    elif previsao == "Intermediario" and historico_lesao == "Sim":
         treino = pd.read_csv("INTERMEDIÁRIO COM LESÃO.csv")
         st.write(treino)
 
-    elif previsao == "iniciante" and historico_lesao == "Não":
+    elif previsao == "Iniciante" and historico_lesao == "Não":
         treino = pd.read_csv("INICIANTE SEM LESÃO.csv")
         st.write(treino)
 
-    elif previsao == "iniciante" and historico_lesao == "Sim":
+    elif previsao == "Iniciante" and historico_lesao == "Sim":
         treino = pd.read_csv("INICIANTE COM LESÃO.csv")
         st.write(treino)
